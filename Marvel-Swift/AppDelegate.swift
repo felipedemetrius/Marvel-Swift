ffc7265625eb280f604f9058a8f6a588fa71b0f7
Felipe Silva <felipe.demetrius@gmail.com>
2020-01-17 22:36:45 -0300

    Add project

---
+++ Marvel-Swift/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Marvel-Swift
+//
+//  Created by Felipe Silva  on 17/01/20.
+//  Copyright © 2020 Felipe Silva . All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ SDKMarvel/SDKMarvel.h
@@ -0,0 +1,19 @@
+//
+//  SDKMarvel.h
+//  SDKMarvel
+//
+//  Created by Felipe Silva  on 17/01/20.
+//  Copyright © 2020 Felipe Silva . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKMarvel.
+FOUNDATION_EXPORT double SDKMarvelVersionNumber;
+
+//! Project version string for SDKMarvel.
+FOUNDATION_EXPORT const unsigned char SDKMarvelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKMarvel/PublicHeader.h>
+
+
